5099d1b892ebb8373a687e25fa407a266c7da2ce
Wiktoria Marcinkowska <73126@student.ue.poznan.pl>
2023-06-03 10:49:24 +0200

    poprawione testy kola

--- kola.py
+++ kola.py
@@ -2,18 +2,18 @@ import math
 
 def objetoscKuli(promien):
     objetosc = (4/3)* math.pi * promien**3
-    return objetosc
+    return round(objetosc, 2)
 
 promienKuli = 4
 objetosc = objetoscKuli(promienKuli)
 
 print(objetosc)
 
-assert objetoscKuli(promienKuli) == 268.082573106329
+assert objetoscKuli(promienKuli) == 268.08
 
 promienKuli_2 = 12
 objetosc_2 = objetoscKuli(promienKuli_2)
 
 print(objetosc_2)
 
-assert objetoscKuli(promienKuli_2) == 7238.229473870882
+assert objetoscKuli(promienKuli_2) == 7238.23
