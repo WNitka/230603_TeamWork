778ea8c221c195cce43c8cca2edc9910ae61ef8f
Alicja Radke <alicja.radke0306@gmail.com>
2023-06-03 10:31:01 +0200

    Poprawiono zaokrąglenia

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 def pole_powierzchni(bok):
     pole_pow = 0
     pole_pow = 6*bok*bok
-    return pole_pow
+    pole_pow_za = round(pole_pow,2)
+    return pole_pow_za
 
 def objetosc_szescian(bok):
     objetosc = 0
     objetosc = bok*bok*bok
-    return objetosc
+    objetosc_za = round(objetosc, 2)
+    return objetosc_za
 
 bok = 5
-pole_pow = pole_powierzchni(bok)
-objetosc = objetosc_szescian(bok)
+pole_pow_za = pole_powierzchni(bok)
+objetosc_za = objetosc_szescian(bok)
 
-print(pole_pow)
-print(objetosc)
+print(pole_pow_za)
+print(objetosc_za)
