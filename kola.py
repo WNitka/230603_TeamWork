46666b0335d032c3e1303dfabee4594c7da775eb
Wiktoria Nitka <wiktoriamnitka.gmail.com>
2023-06-03 10:25:49 +0200

    Zaokrąglanie wyników funkcji

--- kola.py
+++ kola.py
@@ -2,18 +2,21 @@ import math
 
 def objetoscKuli(promien):
     objetosc = (4/3)* math.pi * promien**3
-    return objetosc
+    objetosc_za = round(objetosc, 2)
+    return (objetosc_za)
 
 promienKuli = 4
 objetosc = objetoscKuli(promienKuli)
 
 print(objetosc)
 
-assert objetoscKuli(promienKuli) == 268.082573106329
+assert objetoscKuli(promienKuli) == 268.08
 
 promienKuli_2 = 12
 objetosc_2 = objetoscKuli(promienKuli_2)
 
 print(objetosc_2)
 
-assert objetoscKuli(promienKuli_2) == 7238.229473870882
+assert objetoscKuli(promienKuli_2) == 7238.23
+
+
