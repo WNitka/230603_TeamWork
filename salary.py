879a31baebd96c822658fcf1de7cd726386f12e7
Alicja Radke <alicja.radke0306@gmail.com>
2023-06-23 20:54:05 +0200

    dodano koszty

--- salary.py
+++ salary.py
@@ -38,3 +38,42 @@ class Pracownik:
             skladki_fep
         )
         return skladki
+
+    def oblicz_koszty(self):
+        skladki_zus_emerytalne = self.wynagrodzenie_brutto * 0.0976
+        skladki_zus_rentowe = self.wynagrodzenie_brutto * 0.015
+        skladki_zus_chorobowe = self.wynagrodzenie_brutto * 0.0245
+        skladki_ubezpieczenie_zdrowotne = self.wynagrodzenie_brutto * 0.09
+        skladki_fundusz_pracy = self.wynagrodzenie_brutto * 0.0245
+        skladki_fgsp = self.wynagrodzenie_brutto * 0.001
+        skladki_fep = self.wynagrodzenie_brutto * 0.015
+        koszty = (
+            skladki_zus_emerytalne +
+            skladki_zus_rentowe +
+            skladki_zus_chorobowe +
+            skladki_ubezpieczenie_zdrowotne +
+            skladki_fundusz_pracy +
+            skladki_fgsp +
+            skladki_fep
+        )
+        return koszty
+
+    def oblicz_koszt_pracodawcy(self):
+        koszty = self.oblicz_koszty()
+        return self.wynagrodzenie_brutto + koszty
+   
+    def oblicz_koszt_calkowity(nazwa_pliku):
+        koszt_calkowity = 0
+
+        with open(nazwa_pliku, 'r', newline='') as plik_csv:
+            czytnik = csv.reader(plik_csv)
+            next(czytnik)
+
+            for row in czytnik:
+                imie, nazwisko, wynagrodzenie_brutto = row
+                pracownik = Pracownik(imie, nazwisko, wynagrodzenie_brutto)
+                koszt_pracownika = pracownik.oblicz_koszty()
+                koszt_calkowity += koszt_pracownika
+
+        return koszt_calkowity
+    
